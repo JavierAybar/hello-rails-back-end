e6163e3d71c3563f9d345d1feab783bfd21a61fc
JavierAybar <129561152+JavierAybar@users.noreply.github.com>
2023-11-30 12:57:19 -0300

    Seyup Linter /Fix Linters

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,6 +1,11 @@
-class Api::MessagesController < ApplicationController
-  def index
+# frozen_string_literal: true
+
+module Api
+  # Controller for handling messages in the API
+  class MessagesController < ApplicationController
+    def index
       message = Message.order('RANDOM()').first
       render json: message
+    end
   end
 end
